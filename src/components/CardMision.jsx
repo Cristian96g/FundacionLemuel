876e24482cb5e768c38600775b054d7e26e0bc4d
David Benigno Viñales Nadal <114771372+DavidNadal00@users.noreply.github.com>
2024-06-20 14:31:49 -0300

    home modificaciones

--- src/components/CardMision.jsx
+++ src/components/CardMision.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { FaRobot } from "react-icons/fa";
 
-const CardMision = ({text, title}) => {
+const CardMision = ({ text, title, icon: Icon }) => {
   return (
-    <div
-        class="block rounded-xl border border-orange-100 p-8 shadow-xl transition hover:border-orange-500/10 hover:shadow-orange-500/10"
-      >
-        <FaRobot size={44} className=' text-orange-500'/>
-        <h2 class="mt-4 text-xl font-bold text-black">{title}</h2>
-        <p class="mt-1 text-sm text-gray-600">
-          {text}
-        </p>
-      </div>
+    <div className="block rounded-xl border border-orange-100 p-8 shadow-xl transition hover:border-orange-500/10 hover:shadow-orange-500/10 text-center">
+      <Icon size={44} className="mx-auto text-orange-500" />
+      <h2 className="mt-4 text-xl font-bold text-headingColor">{title}</h2>
+      <p className="mt-1 text-sm text-headingColor">
+        {text}
+      </p>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import ngo2 from '../assets/ngo6.jpg'
-import ngo3 from '../assets/ngo4.jpg'
+import home1 from '../assets/home1.jpg'
+import home2 from '../assets/home2.jpg'
+import home3 from '../assets/home3.jpg'
 import CardMision from '../components/CardMision';
 import Button from '../components/Button';
 import imgdonate from '../assets/imgDonate.jpg';
 import { truncateText } from '../constants/index';
 import informationCard from "../constants/informationCardHome.js"
 import ContactForm from '../components/ContactForm.jsx';
+import { FaSchool, FaHeart, FaSmile, FaGift } from 'react-icons/fa';
 
 
 
@@ -25,9 +27,11 @@ const Home = () => {
                     Brindando <span className="text-[#f58634]">esperanza y apoyo</span> a la comunidad
                   </h1>
 
-                  <p className="text__para text-center md:text-start">
-                    Una organización comprometida a extender ayuda a niños y adultos en momentos difíciles.
-                    Una organización comprometida a extender ayuda
+                  <p className="text__para text-center md:text-justify text-headingColor">
+                    Una organización comprometida a brindar ayuda y contención a niños, adolescentes, jóvenes y adultos
+                    en situación de vulnerabilidad social, así como también a ofrecer apoyo en momentos difíciles de la vida.
+                    Nuestro objetivo es consolidar y afianzar al ser humano, trabajando de manera integral para que cada individuo
+                    pueda alcanzar la plenitud deseada.
                   </p>
                   <Button className={'text-white bg-[#f58634] mt-[24px]'} text="Ver más" />
                 </div>
@@ -36,12 +40,12 @@ const Home = () => {
               {/* ============== Hero content ============== */}
               <div className="flex gap-[30px] justify-end">
                 <div>
-                  <img className="w-full" src={ngo2} alt="" />
+                  <img className="w-full" src={home1} alt="" />
                 </div>
 
                 <div className="mt-[30px] hidden md:block ">
-                  <img src={ngo3} className="w-full mb-[30px]" alt="" />
-                  <img src={ngo3} className="w-full" alt="" />
+                  <img src={home2} className="w-full mb-[30px]" alt="" />
+                  <img src={home3} className="w-full" alt="" />
                 </div>
               </div>
             </div>
@@ -54,23 +58,32 @@ const Home = () => {
       <section class=" text-black">
         <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-4 lg:pt-0">
           <div class="mx-auto max-w-[36rem] text-center">
-            <h2 class="text-3xl font-bold sm:text-4xl text-black">Estamos En Una Misión De Ayudar</h2>
-            <p class="mt-4 text-gray-600">
-              Fundación Lemuel es una entidad de bien público, sin fines de lucro, creada por la iniciativa de la Pastora Viviana Safita de Villarreal, con la misión de extender ayuda en sentido social, en especial a niños y ancianos en situación de riesgo.
+            <h2 class="text-3xl font-bold sm:text-4xl text-headingColor">Estamos En Una Misión De Ayudar</h2>
+            <p class="mt-4 text-headingColor">
+              La Fundación Lemuel es una entidad de bien público sin fines de lucro, creada por la iniciativa de la Pastora Viviana Safita de Villarreal.
+              Nuestra misión es ofrecer ayuda social a quienes más lo necesitan. Conoce algunas de nuestras campañas.
             </p>
           </div>
-          <div class="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <div class="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
             <CardMision
-              title="Atención Integral a Niños"
-              text="Brindamos cuidado y apoyo integral a niños en situación de riesgo, asegurando su bienestar físico, emocional y espiritual."
+              title="Más Útiles Que Nunca"
+              text="Recolectamos útiles escolares para niños y jóvenes de comunidades vulnerables, apoyando su educación."
+              icon={FaSchool}
             />
             <CardMision
-              title="Asistencia a Ancianos"
-              text="Ofrecemos acompañamiento y asistencia a ancianos que se encuentran en situaciones de vulnerabilidad, proporcionando atención y cuidado personalizado."
+              title="Ayúdanos a Abrigar"
+              text="Juntamos ropa de invierno y mantas para quienes más lo necesitan durante los meses fríos, ayudándolos a mantenerse abrigados."
+              icon={FaHeart}
             />
             <CardMision
-              title="Desarrollo Comunitario"
-              text="Trabajamos en proyectos de desarrollo comunitario para mejorar la calidad de vida de las personas en nuestras comunidades, promoviendo el bienestar y la inclusión social."
+              title="Juntos por una Sonrisa"
+              text="Celebramos el Día del Niño recolectando juguetes y regalos para llevar sonrisas a los niños en su día especial."
+              icon={FaSmile}
+            />
+            <CardMision
+              title="Navidad de Jesús"
+              text="Llevamos cajas navideñas a distintos lugares para compartir la Navidad y llevar alegría a más familias."
+              icon={FaGift}
             />
           </div>
         </div>
@@ -81,28 +94,27 @@ const Home = () => {
       <section class="bg-white ">
         <div class="container px-6 py-10 mx-auto">
           <div class="text-center">
-            <h1 class="text-3xl font-bold sm:text-4xl text-black">Actividades Recientes</h1>
+            <h1 class="text-3xl font-bold sm:text-4xl text-headingColor">Actividades Recientes</h1>
 
-            <p class="max-w-lg mx-auto mt-4 text-gray-500">
-              Salami mustard spice tea fridge authentic Chinese food dish salt tasty liquor. Sweet savory foodtruck
-              pie.
+            <p class="max-w-lg mx-auto mt-4 text-headingColor">
+              Las últimas novedades de nuestra fundación del mes de mayo
             </p>
           </div>
 
-          <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3">
+          <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-4">
             {informationCard.map((information) => (
               <div>
                 <div class="relative">
                   <img class="object-cover object-center w-full h-64 rounded-lg lg:h-80" src={information.img} alt="" />
                 </div>
 
-                <h1 class="mt-6 text-xl font-semibold text-gray-800 ">
+                <h1 class="mt-6 text-xl font-semibold text-headingColor ">
                   {information.title}
                 </h1>
 
                 <hr class="w-32 my-3 text-orange-500" />
 
-                <p class="text-sm text-gray-500 ">
+                <p class="text-sm text-justify text-headingColor ">
                   {truncateText(information.text, 140)}
                 </p>
 
@@ -114,7 +126,7 @@ const Home = () => {
           {/* bg-[#f5f6fa] */}
         </div>
         <div className="container">
-          <div className='flex flex-col rounded-lg  border shadow-lg shadow-[rgba(251,146,60,0.24)] border-orange-100 mt-12 md:mt-[6rem] md:flex-row'>
+          <div className='flex flex-col bg-[#f5f6fa] rounded-lg border shadow-lg shadow-[rgba(251,146,60,0.24)] border-orange-100 mt-12 md:mt-[6rem] md:flex-row'>
             <div className="relative w-[100%] h-auto md:rounded-l-lg md:rounded-tr-none rounded-t-lg bg-gradient-to-b from-black to-orange-900 md:w-3/5">
               <img className="md:absolute md:h-full h-[25rem] w-full object-cover md:rounded-l-lg md:rounded-tr-none rounded-t-lg opacity-30" src={imgdonate} alt="" />
               <div className='absolute w-[100%] px-8 py-14 md:p-0 md:w-3/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
